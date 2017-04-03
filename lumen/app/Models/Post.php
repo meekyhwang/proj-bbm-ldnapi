9563768801df4c791cfa6f5fa538601fd5ebc73f
Meeky Hwang <meekyhwang@gmail.com>
2017-04-03 16:55:59 -0400

    resolves template bundle to template

--- lumen/app/Models/Post.php
+++ lumen/app/Models/Post.php
@@ -35,26 +35,75 @@ class Post
         $meta = $this->getFieldsByPostId($post_id);
         $metaArray = [ ];
         foreach($meta as $meta_item){
-            if($meta_item->meta_key == 'template_list' || $meta_item->meta_key == 'repeat'){
-                $metaArray[$meta_item->meta_key] = unserialize($meta_item->meta_value);
+            if($meta_item->meta_key == 'template_list'){
+                $metaArray[$meta_item->meta_key] = $this->resolveTemplateBundles(unserialize($meta_item->meta_value));
+            }elseif($meta_item->meta_key == 'repeat'){
+	            $metaArray[$meta_item->meta_key] = unserialize($meta_item->meta_value);
             }else{
-                $metaArray[$meta_item->meta_key] = $meta_item->meta_value;
+	            $metaArray[$meta_item->meta_key] = $meta_item->meta_value;
             }
         }
-
         $postArray['meta'] = $metaArray;
 
         $return = new Collection($postArray);
         return $return;
     }
 
-    protected function getFieldsByPostId($post_id ='')
+	/**
+	 * @param string $post_id
+	 *
+	 * @return mixed
+	 */
+    protected function getFieldsByPostId($post_id ='',$meta_key='')
     {
+	    $additional_where ='';
+    	if($meta_key !== ''){
+    	    $additional_where = ' AND meta_key = "'.$meta_key.'"';
+	    }
+
         $fields = DB::table('wp_postmeta')
         ->select('meta_key', 'meta_value')
-        ->whereRaw('post_id="'.$post_id.'" AND meta_key NOT REGEXP "^(field_|_).*"')
+        ->whereRaw('post_id="'.$post_id.'" AND meta_key NOT REGEXP "^(field_|_).*"' . $additional_where)
         ->get();
 
-            return $fields;
+	    return $fields;
+    }
+
+	/**
+	 * @param string $post_id
+	 *
+	 * @return mixed
+	 */
+    protected function verifyPostType($post_ids)
+    {
+
+	    $post_types = DB::table('wp_posts')
+		    ->select('ID','post_type')
+		    ->whereIn('ID', $post_ids)//'['.implode(',',$post_ids).']')
+		    ->get();
+
+	    return $post_types;
+    }
+
+	/**
+	 * @param $template_lists
+	 *
+	 * @return array
+	 */
+    protected function resolveTemplateBundles($template_lists)
+    {
+    	$final_template_list = [ ];
+    	$post_types = $this->verifyPostType($template_lists);
+		foreach($post_types as $post){
+			if('template_bundle' == $post->post_type){
+				$template_bundle_list = $this->getFieldsByPostId($post->ID, 'template_list');
+
+				$final_template_list = array_merge($final_template_list, unserialize($template_bundle_list[0]->meta_value));
+			}else{
+				$final_template_list[] = $post->ID;
+			}
+		}
+
+		return $final_template_list;
     }
 }
